98b591708085df7bb16681c09ce63cb21af99fcd
duartebranco <duartebranco35@gmail.com>
2025-12-09 17:23:35 +0000

    fix build.gradle mqtt broker port

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         // Read from local.properties and expose to BuildConfig
         buildConfigField("String", "MAPTILER_API_KEY", "\"${localProperties.getProperty("MAPTILER_API_KEY", "")}\"")
         buildConfigField("String", "MQTT_BROKER_ADDRESS", "\"${localProperties.getProperty("MQTT_BROKER_ADDRESS", "192.168.1.201")}\"")
-        buildConfigField("String", "MQTT_BROKER_PORT", localProperties.getProperty("MQTT_BROKER_PORT", "1884"))
+        buildConfigField("String", "MQTT_BROKER_PORT", "\"${localProperties.getProperty("MQTT_BROKER_PORT", "1884")}\"")
     }
 
     buildTypes {

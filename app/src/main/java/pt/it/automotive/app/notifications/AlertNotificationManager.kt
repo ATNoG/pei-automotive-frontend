b84323922d2c33985f02f1159a2ea5a36827de77
FilipeNV1 <filipeviseu@ua.pt>
2026-04-29 16:43:10 +0100

    ups, error

--- app/src/main/java/pt/it/automotive/app/notifications/AlertNotificationManager.kt
+++ app/src/main/java/pt/it/automotive/app/notifications/AlertNotificationManager.kt
@@ -229,7 +229,7 @@ class AlertNotificationManager(
                 firstNewAlert = false
                 speakForAlert(
                     AlertPreferenceManager.AlertType.WEATHER,
-                    context.getString(R.string.weather_alert_tts, alert.event)
+                    "Weather alert: ${alert.event}"
                 )
             }
         }

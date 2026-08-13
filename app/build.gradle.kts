2290adcc720503107791382bb938fdef5c7983d6
Filipe Viseu <144609567+FilipeNV1@users.noreply.github.com>
2026-08-13 10:46:01 +0100

    Update versionCode and versionName to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "it.av.pt.automotive_app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1.1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
